3c5a061a1f318d92d09b7dbc53ccf3652eef62d0
Nick Wang <nickyiliwang@gmail.com>
2020-03-03 14:40:35 -0500

    rendering content

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# 2020 Internship Menu Exercise
+# About me
+
+## Work / Project Examples
+
+Provide up to three examples of work/projects that you've recently participated in and provide detailed information about your role and contributions to the project.
+
+## Inspiration
+
+Provide up to three examples of work that has recently inspired you from a technical perspective. These can be open source projects, CodePens/CodeSandboxes, blog posts, etc. Be sure to include specifics on what you found inspiring.
+
+## Focus
+
+If you were given the time and resources to build/learn anything you wanted during your internship, where would you choose to focus your efforts? Provide a brief description of how you would spend your time.
+
+# Code Challenge
+
+## 2020 Internship Menu Exercise
 
 Tackle the following steps to create a menu for a "Fancy Italian Restaurant".
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import "./styles.css";
 import menu from "./menu";
-
+// DOM Selects
+const starters = document.querySelector(".starters-content");
+const pasta = document.querySelector(".pasta-content");
+const pizza = document.querySelector(".pizza-content");
+// menu database
 const menuItems = menu.items;
 
-console.log(menuItems);
+const menuFilterAndSort = itemName => {
+  const filterResult = menuItems.filter(item => item.type === itemName);
+  const sortedFilterResults = filterResult.sort(
+    (a, b) => a.menuOrder - b.menuOrder
+  );
+
+  return sortedFilterResults;
+};
+
+const startersResults = menuFilterAndSort("starters");
+const pastaResults = menuFilterAndSort("pasta");
+const pizzaResults = menuFilterAndSort("pizza");
+
+startersResults.forEach(
+  ({ name, description, type, price, spicy, menuOrder }) => {
+    let singleList = document.createElement("li");
+    singleList.innerHTML = `
+        <h2>${name}</h2>
+        <p>${description}</p>
+        <p>${type}</p>
+        <p>$ ${price}</p>
+        <p>Spicy? ${spicy}</p>
+`;
+
+    starters.appendChild(singleList);
+  }
+);
